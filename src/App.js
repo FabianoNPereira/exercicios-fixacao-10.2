a7d34d097892f0fd0f8fede608c5d907aa9436e1
Fabiano Pereira <fabiano.n.pereira@gmail.com>
2022-05-08 22:08:54 -0300

    Exercicio de fixacao propTypes

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
-import React from 'react';
-import Order from './components/Order';
+import Greeting from './components/Greeting';
 
-class App extends React.Component {
-  render() {
-    const orders = [
-      {
-        id: 102,
-        user: "cena@gmail.com",
-        product: "Razer Headphone",
-        price: {
-          value: 99.99,
-          currency: "dollars"
-        },
-      },
-      {
-        id: 77,
-        user: "cena@gmail.com",
-        product: "Monster 500mL",
-        price: {
-          value: 9.99,
-          currency: "dollars"
-        }
-      }
-    ];
-
-    return (
-      <div className="App">
-        <h1> Orders recently created </h1>
-        {orders.map((order) => 
-          <li key={order.id}>
-          <Order order={order} />
-          </li>
-        )}
-      </div>
-    );
-  }
+function App() {
+  return (
+    <main>
+      <Greeting name="Samuel" lastName="Silva" />
+    </main>
+  );
 }
 
 export default App;
